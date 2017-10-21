77008bef38a66daadc25f64d470225c443469385
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2017-10-21 14:42:51 +0300

    Add missing <string.h>

--- connection.c
+++ connection.c
@@ -1,5 +1,6 @@
 #include <assert.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
--- dfa.c
+++ dfa.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <string.h>
 #include <strings.h>
 #include <syslog.h>
 #include "dfa.h"
