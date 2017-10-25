29baf696753ccf818919fb8511f7b999e2332b32
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2017-10-25 22:35:09 +0300

    Create socket before daemonization

--- main.c
+++ main.c
@@ -73,32 +73,28 @@ static void become_daemon(struct globals_t* g)
     }
 }
 
-static int main_loop(struct globals_t* g)
+static void create_socket(struct globals_t* g)
 {
     const int on = 1;
-    ev_signal sigterm_watcher;
-    ev_signal sigint_watcher;
-    ev_signal sigquit_watcher;
-    ev_io accept_watcher;
     struct sockaddr_in sin;
     int res;
 
     sin.sin_port = htons((uint16_t)atoi(g->bind_port));
-    if (1 == inet_pton(AF_INET,  g->bind_address, &sin.sin_addr)) {
+    if (1 == inet_pton(AF_INET, g->bind_address, &sin.sin_addr)) {
         sin.sin_family = AF_INET;
     }
-    else if (1 == inet_pton(AF_INET6,  g->bind_address, &sin.sin_addr)) {
+    else if (1 == inet_pton(AF_INET6, g->bind_address, &sin.sin_addr)) {
         sin.sin_family = AF_INET6;
     }
     else {
-        syslog(LOG_DAEMON | LOG_CRIT, "ERROR: '%s' is not a valid address\n", g->bind_address);
-        return EXIT_FAILURE;
+        fprintf(stderr, "ERROR: '%s' is not a valid address\n", g->bind_address);
+        exit(EXIT_FAILURE);
     }
 
     g->socket = socket(AF_INET, SOCK_STREAM, 0);
     if (-1 == g->socket) {
-        syslog(LOG_DAEMON | LOG_CRIT, "ERROR: Failed to create socket: %m");
-        return EXIT_FAILURE;
+        fprintf(stderr, "ERROR: Failed to create socket: %s", strerror(errno));
+        exit(EXIT_FAILURE);
     }
 
     setsockopt(g->socket, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(int));
@@ -106,15 +102,23 @@ static int main_loop(struct globals_t* g)
 
     res = bind(g->socket, (struct sockaddr*)&sin, sizeof(sin));
     if (-1 == res) {
-        syslog(LOG_DAEMON | LOG_CRIT, "ERROR: failed to bind(): %m");
-        return EXIT_FAILURE;
+        fprintf(stderr, "ERROR: failed to bind(): %s", strerror(errno));
+        exit(EXIT_FAILURE);
     }
 
     res = listen(g->socket, 1024);
     if (-1 == res) {
-        syslog(LOG_DAEMON | LOG_CRIT, "ERROR: failed to listen(): %m");
-        return EXIT_FAILURE;
+        fprintf(stderr, "ERROR: failed to listen(): %s", strerror(errno));
+        exit(EXIT_FAILURE);
     }
+}
+
+static int main_loop(struct globals_t* g)
+{
+    ev_signal sigterm_watcher;
+    ev_signal sigint_watcher;
+    ev_signal sigquit_watcher;
+    ev_io accept_watcher;
 
     ev_signal_init(&sigterm_watcher, signal_callback, SIGTERM);
     ev_signal_init(&sigint_watcher,  signal_callback, SIGINT);
@@ -142,6 +146,7 @@ int main(int argc, char** argv)
     parse_options(argc, argv, &globals);
     openlog(globals.daemon_name, option, LOG_DAEMON);
     check_pid_file(&globals);
+    create_socket(&globals);
     become_daemon(&globals);
 
     if (write_pid(globals.pid_fd)) {
